8617f6bf6f74fd5d2f6cb3d3cf841827e293ffc5
Mark Paterson <mark.paterson@nasa.gov>
2019-12-11 13:41:07 -0600

    Fix CI test breakages
    - Duplicate coeff in v_d_001.xml
    - Missing "SpringStiffness" as a valid coeff name

--- src/val_description/CoeffSchemaDefinitions.py
+++ src/val_description/CoeffSchemaDefinitions.py
@@ -28,6 +28,7 @@ actuator_coeffs_definition = \
         <xs:enumeration value="TorqueOffset_Nm"/>
         <xs:enumeration value="ForceOffset_N"/>
         <xs:enumeration value="JointGearRatio"/>
+        <xs:enumararion_value="SpringStiffness"/>
     </xs:restriction>
 </xs:simpleType>
 '''
